c6ec51f2b9344773b931a36d023176aac50d9b1c
Prabhakar Patil <prabhakarpatil648@gmail.com>
2024-11-12 23:41:24 +0530

    Initial Commit

---
+++ UpCryptosTests/UpCryptosTests.swift
@@ -0,0 +1,17 @@
+//
+//  UpCryptosTests.swift
+//  UpCryptosTests
+//
+//  Created by Prabhakar Patil on 12/11/24.
+//
+
+import Testing
+@testable import UpCryptos
+
+struct UpCryptosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
